b43dc6f0779ef08bf343977adebe4e700dfce66b
Dmitry Shestak <dimonshestak160201@gmail.com>
2022-02-28 19:42:01 +0300

    save

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,65 @@
-module.exports.login = function( req, res ){
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
-        }
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/User')
+const keys = require('../config/keys')
+
+module.exports.login = async function( req, res ){
+    const candidate = await User.findOne({
+        email: req.body.email
     })
+
+    if( candidate ){
+        const passwordResult = bcrypt.compareSync( req.body.password, candidate.password )
+
+        if( passwordResult ){
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {
+                expiresIn: 60 * 60
+            })
+
+            res.status( 200 ).json({
+                token: `Bearer ${token}`
+            })
+        }else{
+            res.status( 401 ).json({
+                message: 'Incorrent password'
+            })
+        }
+    }else{
+        res.status( 404 ).json({
+            message: 'User not found'
+        })
+    }
 }
 
 
-module.exports.register = function( req, res ){
-    res.status(200).json({
-        register: true
+module.exports.register = async function( req, res ){
+    const candidate = await User.findOne({
+        email: req.body.email
     })
+    
+    if( candidate ){
+        res.status( 409 ).json({
+            message: 'This email is exist'
+        })
+    }else{
+
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync( password, salt )
+        })
+
+        try{
+            await user.save()
+            res.status( 201 ).json( user )
+        }catch( error ){
+            // Error
+        }
+
+    }
 }
